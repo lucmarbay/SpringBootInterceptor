dec953c6ee07d52e2f4f69059f90ac531009d32c
lucmarbay <lukimartinezbayon@gmail.com>
2019-11-11 17:09:59 +0100

    Añadido el pre y el post de los interceptors

--- src/main/java/com/everis/demo/interceptor/ProductServiceInterceptor.java
+++ src/main/java/com/everis/demo/interceptor/ProductServiceInterceptor.java
@@ -1,6 +1,10 @@
 package com.everis.demo.interceptor;
 
 import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.Enumeration;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -9,22 +13,59 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Component
 public class ProductServiceInterceptor implements HandlerInterceptor {
-   public boolean preHandle
-      (HttpServletRequest request, HttpServletResponse response, Object handler) 
-      throws Exception {
-      
-      System.out.println("Pre Handle method is Calling");
-      return true;
-   }
-   public void postHandle(HttpServletRequest request, HttpServletResponse response, 
-      Object handler, ModelAndView modelAndView) throws Exception {
-      
-      System.out.println("Post Handle method is Calling");
-   }
-   public void afterCompletion
-      (HttpServletRequest request, HttpServletResponse response, Object 
-      handler, Exception exception) throws Exception {
-      
-      System.out.println("Request and Response is completed");
-   }
+
+	String cabecera = "";
+	String valorCabecera = "";
+
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+
+		Enumeration en = request.getHeaderNames();
+		while (en.hasMoreElements()) {
+
+			String headerName = (String) en.nextElement();
+
+			String headerValue = request.getHeader(headerName);
+			if (headerName.equals("valor1")) {
+				System.out.print("Cabecera de la peticion: Header Name = " + headerName + " " + " Header Value = "
+						+ headerValue + ". ");
+				this.cabecera = headerName;
+				this.valorCabecera = headerValue;
+			}
+		}
+
+		System.out.println("Pre Handle method is Calling");
+		return true;
+	}
+
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		response.setHeader(cabecera, valorCabecera + "Modificado");
+//      Enumeration en2 = (Enumeration) response.getHeaderNames();
+//	    while(en2.hasMoreElements()){
+//
+//	        String headerName = (String) en2.nextElement(); 
+//
+//	        String headerValue = response.getHeader(headerName);
+//	        if(headerName.equals("valor1")) {
+//	        	System.out.print("Cabecera de la respuesta: Header Name = "+ headerName + " " + " Header Value = "+ headerValue + ". ");
+//	        }
+//	    }
+		
+		//Esto da un problema 
+		Collection<String> headerNames = response.getHeaderNames();
+
+		while (!headerNames.isEmpty()) {
+			String headerName = (String) headerNames.toString();
+
+			System.out.println("Cabecera de la respuesta: Header Name = "+headerName + " Header Value = " + response.getHeader(headerName));
+		}
+		System.out.println("Post Handle method is Calling");
+	}
+
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
+			Exception exception) throws Exception {
+
+		System.out.println("Request and Response is completed");
+	}
 }
