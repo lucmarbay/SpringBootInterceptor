0b3bd16551e5d06f1b6c8b2b238d7a262ad70582
lucmarbay <lukimartinezbayon@gmail.com>
2019-11-12 11:28:21 +0100

    añadido el @RequestHeader para obtener un parametro de la cabecera y modificarlo

--- src/main/java/com/everis/demo/interceptor/ProductServiceInterceptor.java
+++ src/main/java/com/everis/demo/interceptor/ProductServiceInterceptor.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 import java.util.Enumeration;
+import java.util.Iterator;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,25 +15,9 @@ import org.springframework.web.servlet.ModelAndView;
 @Component
 public class ProductServiceInterceptor implements HandlerInterceptor {
 
-	String cabecera = "";
-	String valorCabecera = "";
 
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-
-		Enumeration en = request.getHeaderNames();
-		while (en.hasMoreElements()) {
-
-			String headerName = (String) en.nextElement();
-
-			String headerValue = request.getHeader(headerName);
-			if (headerName.equals("valor1")) {
-				System.out.print("Cabecera de la peticion: Header Name = " + headerName + " " + " Header Value = "
-						+ headerValue + ". ");
-				this.cabecera = headerName;
-				this.valorCabecera = headerValue;
-			}
-		}
 
 		System.out.println("Pre Handle method is Calling");
 		return true;
@@ -40,26 +25,8 @@ public class ProductServiceInterceptor implements HandlerInterceptor {
 
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
-		response.setHeader(cabecera, valorCabecera + "Modificado");
-//      Enumeration en2 = (Enumeration) response.getHeaderNames();
-//	    while(en2.hasMoreElements()){
-//
-//	        String headerName = (String) en2.nextElement(); 
-//
-//	        String headerValue = response.getHeader(headerName);
-//	        if(headerName.equals("valor1")) {
-//	        	System.out.print("Cabecera de la respuesta: Header Name = "+ headerName + " " + " Header Value = "+ headerValue + ". ");
-//	        }
-//	    }
 		
-		//Esto da un problema 
-		Collection<String> headerNames = response.getHeaderNames();
 
-		while (!headerNames.isEmpty()) {
-			String headerName = (String) headerNames.toString();
-
-			System.out.println("Cabecera de la respuesta: Header Name = "+headerName + " Header Value = " + response.getHeader(headerName));
-		}
 		System.out.println("Post Handle method is Calling");
 	}
 
@@ -68,4 +35,5 @@ public class ProductServiceInterceptor implements HandlerInterceptor {
 
 		System.out.println("Request and Response is completed");
 	}
+	
 }
